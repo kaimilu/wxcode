e35271aae3daed2caebb3955d408315b649f2864
suzhihui <wisdom.su@163.com>
2017-12-05 23:47:23 +0800

    Tips Class

---
+++ src/utils/Tips.js
@@ -0,0 +1,16 @@
+/**
+ * 提示与加载工具类
+ */
+export default class Tips {
+  static isLoading = false
+  static pause = false
+
+  /**
+   * 弹出提示框
+   * @param {标题} title 
+   * @param {间隔} duration 
+   */
+  static success (title, duration = 500) {
+
+  }
+}
